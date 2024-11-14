267edc4286b5fac5ca8eaa4b5e11eb9791464df7
Antoine Gros <antoine.gros@map.cnrs.fr>
2024-11-14 20:02:15 +0100

    Init from 3.7

---
+++ index.js
@@ -0,0 +1,123 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+// Middlewares
+var corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions))
+// app.use(express.static('dist'))
+
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+  
+app.use(express.json())
+app.use(requestLogger)
+
+const generateId = () => {
+    return Math.floor(Math.random() * 1000).toString();
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello Phonebook!</h1>')
+})
+
+app.get('/api/info', (request, response) => {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const today  = new Date();
+    response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>At time : ${today.toLocaleDateString("en-US", options)} </p>`)
+    // response.json(persons)
+})
+
+//  /api/persons
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        console.log('x')
+        response.status(404).end()
+    }
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name || !body.number ) {
+      return response.status(400).json({ 
+        error: `Name or number is missing, ${body.name} ${body.number}` 
+      })
+    }
+  
+    if(persons.find(person => person.name === body.name)) {
+      return response.status(400).json({ 
+        error:`${body.name} already exists`
+      })
+    }
+  
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: generateId(),
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+})
+
+
+
+// LISTEN //
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
