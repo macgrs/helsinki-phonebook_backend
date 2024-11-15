2896865d620d75ea6b377e49a792eb0b4dfc1c51
Antoine Gros <antoine.gros@map.cnrs.fr>
2024-11-15 16:12:46 +0100

    Added static dist from phonebook frontend

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors(corsOptions))
-// app.use(express.static('dist'))
+app.use(express.static('dist'))
 
 
 let persons = [
